4892eb113e96e63f4f602a9eeceefbbbb8e974cc
Átila Castro <atilacastro@gmail.com>
2019-03-29 16:41:15 -0300

    Change code - COUNT removed

--- migration-to-route53.sh
+++ migration-to-route53.sh
@@ -2,12 +2,9 @@
 
 ZONES=$(cat /teknisa/route53-migration/file.txt)
 ZONES=(${ZONES})
-#ZONEXIST=$(cat /tmp/zonexist)
 
 for LINE in ${ZONES[@]}; do
 ZONEXIST=$(cli53 list | awk '{print "" $2}' | grep -i $LINE | wc -l)
-
-#COUNT=(${ZONEXIST})
 
 if [ "$ZONEXIST" -ge "1" ];
   then
@@ -15,9 +12,7 @@ if [ "$ZONEXIST" -ge "1" ];
 
 elif [ "$ZONEXIST" -eq "0" ];
   then
-           echo $LINE " do not exist "
+           echo $LINE " do not exist, creating... "
            cli53 create $LINE --comment 'Domain'
            cli53 import --file $LINE --replace --wait $LINE
 fi
-#rm -rf $ZONEXIST
-done
